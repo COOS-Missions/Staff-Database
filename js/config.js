af25196bc8f02470d5194289fd5a0b1b12cb56e3
COOS-Missions <missions@coos.org.sg>
2026-09-02 17:46:18 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
     APP_NAME: "Missions Portal",
     VERSION: "1.0.0",
 
-    CLIENT_ID: "263902111764-jdil2ttim5q05gim8tsheopgoprr6jmh.apps.googleusercontent.com",
+    CLIENT_ID: "907761640441-eul5vjjulde6e9a7n0053lt8p1uj5468.apps.googleusercontent.com",
 
     WEB_APP_URL: "https://script.google.com/macros/s/AKfycbx8qNmvblKd_isBS7F_xs2DjU6xDZmu1KhSSSRTa8ThnY01c5W3frUVZbeEGFs9vhTU/exec",
 
