f285af9a73504a892db14f1e5ffb0f0f1a8cd9dc
COOS-Missions <missions@coos.org.sg>
2026-09-02 17:47:58 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,7 +8,7 @@ const CONFIG = {
 
     CLIENT_ID: "907761640441-eul5vjjulde6e9a7n0053lt8p1uj5468.apps.googleusercontent.com",
 
-    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbx8qNmvblKd_isBS7F_xs2DjU6xDZmu1KhSSSRTa8ThnY01c5W3frUVZbeEGFs9vhTU/exec",
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbwUGUzlnhVb3a97-DI_C9_FpuB-fXQg9y7KNU0b1NmP_8p--BnCjIXjE6xG2DcEyrL0/exec",
 
     DEBUG: true
 };
